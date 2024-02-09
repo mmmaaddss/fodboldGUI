741112590b7c5b6957ed7bba36d9cc49cba22d86
mmmaaddss <52237511+mmmaaddss@users.noreply.github.com>
2024-02-09 14:10:54 +0100

    fixet liste, er igang med at lave progress bar

--- listWindow.py
+++ listWindow.py
@@ -1,3 +1,5 @@
+from tkinter.ttk import Progressbar
+
 from PIL import ImageTk, Image  # image stuff - install package: Pillow
 from tkinter import *
 
@@ -13,25 +15,46 @@ class listWindowClass:
         self.topFrame.pack(side=TOP, fill=X)
         Label(self.topFrame, text="Liste over alle fodboldturens deltagere,", font=('Arial 15 bold')).pack(side=LEFT, pady=(0,10) )
 
-        # Left frame
-        self.leftFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
-        self.leftFrame.pack(side=LEFT, fill=BOTH, expand=False)  # Changed expand to False
-        for item in self.master.fodboldtur:
-            Label(self.leftFrame, text=item).pack(side=TOP, anchor='w', padx=(0,15))  # Align labels to the left
+        # Assuming this code is inside a method of a class
+        for key, value in self.master.fodboldtur.items():
+            frame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
+            frame.pack(side=TOP, fill=BOTH, expand=False)
+            leftframe = Frame(frame, borderwidth=2, relief=GROOVE)
+            leftframe.pack(side=LEFT, fill=BOTH, expand=False)
+            leftframe.pack_propagate(False)
+            leftframe.config(width=120)
+            Label(leftframe, text=key).pack(side=TOP, anchor='w')
 
-        # right frame
-        self.rightFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
-        self.rightFrame.pack(side=RIGHT, fill=BOTH, expand=True)
-        #for keys in self.master.fodboldtur.keys():
-         #   self.progress = Progressbar(self.root, orient=HORIZONTAL, length=200, mode='determinate')
-          #  self.progress['value'] = (self.master.fodboldtur.keys() / self.master.personaltarget) * 100
-           # self.progress.pack(pady=10)
+            centerframe = Frame(frame, borderwidth=2, relief=GROOVE)
+            centerframe.pack(fill=BOTH, expand=False)
+            Label(centerframe, text=value).pack(side=TOP, anchor='center')
 
-        # middle frame
-        self.middleFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
-        self.middleFrame.pack(fill=BOTH, expand=True)
-        for key in self.master.fodboldtur.keys():
-            Label(self.middleFrame, text=f'{self.master.fodboldtur[key]} / {self.master.personaltarget}').pack(side=TOP, anchor='w')  # Align labels to the center
+            rightframe = Frame(frame, borderwidth=2, relief=GROOVE)
+            rightframe.pack(side=RIGHT, fill=BOTH, expand=False)
+            self.progress = Progressbar(self.listWindow, orient=HORIZONTAL, length=200, mode='determinate')
+            self.progress['value'] = (self.master.fodboldtur[key] / self.master.personaltarget) * 100
+
+
+    ## Left frame
+     #self.leftFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
+     #self.leftFrame.pack(side=LEFT, fill=BOTH, expand=False)  # Changed expand to False
+     #for item in self.master.fodboldtur:
+     #    Label(self.leftFrame, text=item).pack(side=TOP, anchor='w', padx=(0,15))  # Align labels to the left
+
+     ## middle frame
+     #self.middleFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
+     #self.middleFrame.pack(fill=BOTH, expand=True)
+     #for key in self.master.fodboldtur.keys():
+     #    Label(self.middleFrame, text=f'{self.master.fodboldtur[key]} / {self.master.personaltarget}').pack(side=TOP, anchor='w')  # Align labels to the center
+
+     ## right frame
+     #self.rightFrame = Frame(self.listWindow, borderwidth=2, relief=GROOVE)
+     #self.rightFrame.pack(side=RIGHT, fill=BOTH, expand=True)
+     #for key in self.master.fodboldtur.keys():
+     #    self.progress = Progressbar(self.listWindow, orient=HORIZONTAL, length=200, mode='determinate')
+     #    self.progress['value'] = (self.master.fodboldtur[key] / self.master.personaltarget) * 100
+     #    self.progress.pack(pady=3.5)
+
 
 
 # Example to show how to use this class
